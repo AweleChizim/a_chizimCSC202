733d1b4f74226b51748656eb86884483980ead89
AweleChizim <chizimawele@gmail.com>
2024-05-02 15:50:57 +0100

    (Commit 2)

---
+++ practice4/src/index.ts
@@ -0,0 +1,8 @@
+console.log('testing\n1...\n2...');
+var readlineSync = require('readline-sync');
+var answer;
+
+do{ 
+    answer = readlineSync.question('What should I do? [Type "exit" for me to quit]: '); 
+    console.log('You asked me to '+ answer) 
+}while(answer != 'exit') 
